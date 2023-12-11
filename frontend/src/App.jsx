38a4ac05a8d5b924c8fa1635d749e8c15a9d424c
TheLonelyDude <magnus.kongstad@gmail.com>
2023-12-11 19:58:49 +0100

    Added a simple header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,19 @@ import "./App.css";
 // Component imports
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Header from "./components/Header/Header";
 
 function App() {
+  document
+    .getElementsByTagName("body")[0]
+    .setAttribute(
+      "data-bs-theme",
+      sessionStorage.getItem("theme") || sessionStorage.setItem("theme", "dark")
+    );
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
