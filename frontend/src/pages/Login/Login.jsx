b46c2804be5c06bea426cb3ba9a6397af930418d
MagKon <magnus.kongstad@gmail.com>
2023-12-13 11:50:57 +0100

    Update

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+// import assets
+import "./style/login.css";
+
 function Login({ onLogin }) {
   return (
     <>
-      <h1 onClick={() => onLogin(true)}>Login</h1>
+      <div className="row" style={{ minWidth: "100vw" }}>
+        <div className="col-auto">
+          <div className="background-image container-fluid"></div>
+        </div>
+        <div className="col-auto" style={{ marginTop: "5rem" }}>
+          <div className="row">
+            <div className="col-auto">
+              <h1 onClick={() => onLogin(true)}>Login</h1>
+            </div>
+            <div className="col-auto"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
