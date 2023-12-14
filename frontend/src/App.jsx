19e2aca5a47e9d6e4e70d7c01c258e7b1691622c
TheLonelyDude <magnus.kongstad@gmail.com>
2023-12-14 15:52:34 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // Base imports
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 // Component imports
@@ -14,6 +14,13 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(
     sessionStorage.getItem("token") || false
   );
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      sessionStorage.setItem("token", true);
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   function login(bool) {
     setIsAuthenticated(bool);
