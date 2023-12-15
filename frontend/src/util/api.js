b0da554e99a412b1ce22eff22c727dc6d07f68ad
TheLonelyDude <magnus.kongstad@gmail.com>
2023-12-15 12:35:52 +0100

    Added api util

---
+++ frontend/src/util/api.js
@@ -0,0 +1,73 @@
+const URL = "http://localhost:7007";
+
+function handleHttpErrors(res) {
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
+}
+
+function apiFacade() {
+  const login = async (user, password) => {
+    const options = makeOptions("POST", true, {
+      username: user,
+      password: password,
+    });
+    const res = await fetch(URL + "/api/v1/auth/login", options);
+    const res_1 = await handleHttpErrors(res);
+    return res_1.token;
+  };
+
+  const fetchData = async () => {
+    const options = makeOptions("GET", true);
+    return fetch(URL + "/api/v1/", options).then(handleHttpErrors);
+  };
+
+  const makeOptions = (method, addToken, body) => {
+    var opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    if (addToken && loggedIn()) {
+      opts.headers["Authentication"] = `Baerer ${getToken()}`;
+    }
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+  };
+
+  const getToken = () => {
+    var token;
+
+    if (localStorage.getItem("token")) {
+      token = localStorage.getItem("token");
+    } else if (sessionStorage.getItem("token")) {
+      token = sessionStorage.getItem("token");
+    } else {
+      return null;
+    }
+
+    return token;
+  };
+
+  const loggedIn = () => {
+    const loggedIn = getToken() != null;
+    return loggedIn;
+  };
+
+  return {
+    makeOptions,
+    getToken,
+    loggedIn,
+    login,
+    fetchData,
+  };
+}
+
+const facade = apiFacade();
+export default facade;
