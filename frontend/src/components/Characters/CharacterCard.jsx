4299173798bdefebbf5b5e2aafcdffcc0e14f6ec
Peter537 <pean@outlook.dk>
2023-12-15 21:12:58 +0100

    first version of characters page

---
+++ frontend/src/components/Characters/CharacterCard.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import placeholder from "./assets/placeholder.png";
+
+function CharacterCard({ character, viewCharacter, deleteCharacter }) {
+  return (
+    <div className="border">
+      <img src={character?.img || placeholder} className="card-img-top"></img>
+      <div className="card-body">
+        <h5 className="card-title">{character.name}</h5>
+        <p className="card-text">{character.raceClass}</p>
+      </div>
+      <div>
+        <button
+          className="character-button btn btn-primary"
+          onClick={() => viewCharacter(character.id)}
+        >
+          VIEW
+        </button>
+        <button
+          className="character-button btn btn-primary"
+          onClick={() => alert("Comming soon... (hopefully)")}
+        >
+          EDIT
+        </button>
+        <button
+          className="character-button btn btn-primary"
+          onClick={() => alert("Comming soon... (hopefully)")}
+        >
+          COPY
+        </button>
+        <button
+          className="character-button btn btn-primary"
+          onClick={() => deleteCharacter(character.id)}
+        >
+          DELETE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterCard;
