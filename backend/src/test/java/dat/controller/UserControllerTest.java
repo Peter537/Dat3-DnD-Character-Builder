b5aa555687f632f717660fc06ed98befaad99ca2
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-12-18 13:59:43 +0100

    Added tests for username login, and removed role from create

--- backend/src/test/java/dat/controller/UserControllerTest.java
+++ backend/src/test/java/dat/controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package dat.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dat.config.ApplicationConfig;
-import dat.model.Role;
 import dat.model.User;
 import org.junit.jupiter.api.Test;
 
@@ -46,15 +45,45 @@ public class UserControllerTest extends dat.Test {
     }
 
     @Test
+    public void testLoginUsernameSuccess() {
+        String bodyJson = String.format("{ \"username\": \"%s\", \"password\": \"%s\" }", "user", "user123");
+        given()
+                .body(bodyJson)
+                .when()
+                .post(ApplicationConfig.getBaseURL() + "/auth/login")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testLoginUsernameInvalidPassword() {
+        String bodyJson = String.format("{ \"username\": \"%s\", \"password\": \"%s\" }", "user", "wrongPassword");
+        given()
+                .body(bodyJson)
+                .when()
+                .post(ApplicationConfig.getBaseURL() + "/auth/login")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testLoginUserInvalidUsername() {
+        String bodyJson = String.format("{ \"username\": \"%s\", \"password\": \"%s\" }", "nonExistentUser", "user123");
+        given()
+                .body(bodyJson)
+                .when()
+                .post(ApplicationConfig.getBaseURL() + "/auth/login")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
     public void testCreateUserSuccess() {
         User newUser = new User("newUser@mail.dk", "newUser", "newPassword");
-        Role userRole = new Role("user");
-        newUser.addRole(userRole);
         String bodyJson = OBJECT_MAPPER.createObjectNode()
                 .put("email", newUser.getEmail())
                 .put("username", newUser.getUsername())
                 .put("password", newUser.getPassword())
-                .put("role", userRole.getName())
                 .toString();
         given()
                 .body(bodyJson)
@@ -66,7 +95,7 @@ public class UserControllerTest extends dat.Test {
 
     @Test
     public void testCreateUserWithExistingEmail() {
-        String bodyJson = String.format("{ \"email\": \"%s\", \"username\": \"%s\", \"password\": \"%s\", \"role\": \"%s\" }", "user@mail.dk", "user", "newPassword", "user");
+        String bodyJson = String.format("{ \"email\": \"%s\", \"username\": \"%s\", \"password\": \"%s\" }", "user@mail.dk", "user", "newPassword");
         given()
                 .body(bodyJson)
                 .when()
